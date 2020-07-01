e8d143711886cbd94eca62e75af3d91474d2294e
Thuc Nguyen <thuch.nguyen@yahoo.com>
2020-07-01 15:18:56 -0700

    Minimap implemented by svg, with drag and scroll activity(not complete yet)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+
+// import logo from './logo.svg';
+import Error from "./components/Error";
+import Drawer from "./components/Drawer";
+import Home from "./components/Home";
+import List from "./components/List"
+import Analyze from "./components/Analyze";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    /** =======================================================================
+     * 
+     * LIFECYCLE
+     * 
+     ======================================================================= */
+    constructor(props) {
+        super(props);
+        // this.state = { messages: [] }; // set up react state
+    } 
+
+
+    /** =======================================================================
+     * 
+     * RENDER
+     * 
+     ======================================================================= */
+    render() {
+        return (
+            <BrowserRouter>
+                <div className="app">
+                    <div className="componentDrawer">
+                        <Drawer/>
+                    </div>
+                    <div className="componentContent">
+                        <Switch>
+                            <Route path="/" component={ Home } exact />
+                            <Route path="/list" component={ List } />
+                            <Route path="/project" component={ Analyze } />
+                            <Route component={ Error } />
+                        </Switch>
+                    </div>
+                </div>
+            </BrowserRouter>
+        );
+    }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ src/components/Drawer.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import "../css/Drawer.css";
+
+import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined';
+import ViewListIcon from '@material-ui/icons/ViewList';
+import AssessmentOutlinedIcon from '@material-ui/icons/AssessmentOutlined';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+
+
+class Drawer extends React.Component {
+    /** =======================================================================
+     * 
+     * LIFECYCLE
+     * 
+     ======================================================================= */
+    constructor(props) {
+        super(props);
+    } 
+
+
+    /** =======================================================================
+     * 
+     * RENDER
+     * 
+     ======================================================================= */
+     render() {
+         return (
+            <div className="drawer">
+                <div className="drawerIcon">
+                    <div></div>
+                </div>
+                <div className="drawerTitle">
+                    <p>SpiderSense</p>
+                </div>
+
+                <div className="drawerNavigation">
+                    <NavLink to="/" className="styledNavLink cursorPointer">
+                        <div className="navIcon">
+                            <HomeOutlinedIcon style={{fill: "white"}}/>
+                        </div>
+                        <p className="navText">Home</p>
+                    </NavLink>
+                    <NavLink to="/list" className="styledNavLink cursorPointer">
+                        <div className="navIcon">
+                            <ViewListIcon style={{fill: "white"}}/>
+                        </div>
+                        <p className="navText">List</p>
+                    </NavLink>
+                    <NavLink to="/project" className="styledNavLink cursorPointer">
+                        <div className="navIcon">
+                            <AssessmentOutlinedIcon style={{fill: "white"}}/>
+                        </div>
+                        <p className="navText">Project</p>
+                    </NavLink>
+                </div>
+
+                <div className="fill"></div>
+
+                <div className="drawerLogout">
+                    <div className="styledNavLink cursorPointer">
+                        <div className="navIcon">
+                            <ExitToAppIcon style={{fill: "white"}}/>
+                        </div>
+                        <p className="navText">Logout</p>
+                    </div>
+                </div>
+            </div>
+         );
+     }
+}
+
+export default Drawer;
