a17f68f5afe2051aad156496e6d5398c3c583998
laofuLF <weihuanf@uci.edu>
2020-11-24 17:54:26 -0800

    replaced d3 select, fixed color and backdrop layout

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import * as d3 from 'd3';
 
 import GitHubIcon from '@material-ui/icons/GitHub';
 import ViewModuleIcon from '@material-ui/icons/ViewModule';
@@ -8,14 +7,11 @@ import ViewListIcon from '@material-ui/icons/ViewList';
 import Backdrop from '@material-ui/core/Backdrop';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
-import { spidersenseWorkerUrls } from '../../vars/vars';
 import { shortenGithubUrl } from '../../util/url-parsers';
 import ColorSequence from '../../util/color-sequence';
 import "./../../vars/shared.scss";
 import "./Home.scss";
-import * as constants from './store/constants'
 import { actionCreator } from './store';
-import { logDOM } from '@testing-library/react';
 
 
 class Home extends React.Component {
@@ -30,58 +26,15 @@ class Home extends React.Component {
         // Variables
         this.viewOptions = [];
 
-        // Initalize state
-        // this.state = {
-        //     projects: [],
-        //     currentViewIndex: -1,
-        //     backdropOpen: true
-        // };
-
         // Bind methods
-        // this.updateCurrentViewOption = this.updateCurrentViewOption.bind(this);
         this.onProjectClicked = this.onProjectClicked.bind(this);
         this.openGithubLink = this.openGithubLink.bind(this);
     } 
 
     componentDidMount() {
-        console.log("component did mount: 123123123");
         const { requestAllProjects, projects } = this.props;
-        // console.log(projects);
         requestAllProjects(projects);
     }
-
-    /** =======================================================================
-     * 
-     * METHODS
-     * 
-     ======================================================================= */
-
-    /**
-     * Request from spidersense-worker all available projects. On response, 
-     * update state to retain projects and initialize view options.
-     */
-    // requestAllProjects() {
-    //     let url = spidersenseWorkerUrls.getAllProjects;
-
-    //     fetch(url, {
-    //         method: 'GET'
-    //     }).then((response) => {
-    //         return response.json();
-    //     }).then((data) => {
-    //         console.log("Callback:\n" + JSON.stringify(data));
-
-    //         // Update state to retain projects
-    //         this.setState((state) => ({
-    //             projects: data.projects
-    //         }))
-
-    //         this.initializeViewOptions();
-    //         this.closeBackdrop();
-    //     }).catch((error) => {
-    //         console.error(error);
-    //         this.closeBackdrop();
-    //     });
-    // }
 
     /**
      * Initialize view options: module view or list view. 
@@ -91,17 +44,19 @@ class Home extends React.Component {
      * Set current view index to the module view.
      */
     initializeViewOptions() {
+        // read data from immutable
+        const projects = this.props.projects.toJS()
         // Get color sequence to set background color of module project units
         let colorSequence = new ColorSequence();
-        let sequence = colorSequence.getColorSequence(this.props.projects.length);
-        console.log(this.props.projects);
-        const projects = this.props.projects.toJS()
+        let sequence = colorSequence.getColorSequence(projects.length);
 
         this.viewOptions = [
             {
                 viewName: "module",
-                container: projects.map((p, i) => (
-                    <div className="projectUnit projectUnitModule" onClick={(e) => this.onProjectClicked(p.projectId, e)}>
+                container: 
+                (<div className="moduleWrapper">
+                    {projects.map((p, i) => (
+                    <div key={p.projectId} className="projectUnit projectUnitModule" onClick={(e) => this.onProjectClicked(p.projectId, e)}>
                         <div style={{backgroundColor: sequence[i]}}></div>
                         <div>
                             <p>{p.projectName}</p>
@@ -110,13 +65,14 @@ class Home extends React.Component {
                                 <GitHubIcon onClick={(e) => this.openGithubLink(p.projectLink, e)} />
                             </div>
                         </div>
-                    </div>
-                ))
+                    </div>))}
+                </div>)
+                
             },
             {
                 viewName: "list",
                 container: projects.map((p) => (
-                        <div className="projectUnit projectUnitList" onClick={(e) => this.onProjectClicked(p.projectId, e)}>
+                        <div key={p.projectId} className="projectUnit projectUnitList" onClick={(e) => this.onProjectClicked(p.projectId, e)}>
                             <p>{p.projectName}</p>
                             <p>{shortenGithubUrl(p.projectLink, 50)}</p>
                             <div>
@@ -126,13 +82,6 @@ class Home extends React.Component {
                     ))
             }
         ];
-
-        console.log(this.viewOptions);
-
-        // Update state to set currentViewIndex to module
-        // this.setState((state) => ({
-        //     currentViewIndex: 0
-        // }));
     }
 
     /**
@@ -143,23 +92,13 @@ class Home extends React.Component {
     populateHomeView() {
         this.initializeViewOptions();
 
+        const { currentViewIndex } = this.props;
+
         if (this.viewOptions == null || this.viewOptions === undefined || this.viewOptions.length === 0) {
-            console.log("null view option");
             return;
         }
 
-        // If current view index is module, add moduleWrapper class
-        if (this.props.currentViewIndex === 0) {
-            d3.select(".homeView")
-                .classed("moduleWrapper", true);
-        } else {
-            d3.select(".homeView")
-                .classed("moduleWrapper", false);
-        }
-
-        console.log(this.props.currentViewIndex);
-
-        return this.viewOptions[this.props.currentViewIndex].container;
+        return this.viewOptions[currentViewIndex].container;
     }
 
     /** =======================================================================
@@ -167,16 +106,6 @@ class Home extends React.Component {
      * METHODS - Event Handling
      * 
      ======================================================================= */
-    /**
-     * Update the state to set currentViewIndex to the selected index
-     * @param {number} index The selected view index
-     */
-    // updateCurrentViewOption(index) {
-    //     this.setState((state) => ({
-    //         currentViewIndex: index
-    //     }));
-    // }
-
     /**
      * On click of a project, update cookie's id to the selected project id
      * and navigate to Project component with parameter projectId.
@@ -199,18 +128,13 @@ class Home extends React.Component {
         window.open(projectLink, '_blank');
     }
 
-    // closeBackdrop() {
-    //     this.setState((state) => ({
-    //         backdropOpen: false
-    //     }));
-    // }
-
     /** =======================================================================
      * 
      * RENDER
      * 
      ======================================================================= */
      render() {
+         const { updateCurrentViewOption, currentViewIndex, backdropOpen } = this.props;
          return (
             <div id="home">
                 <div className="toolbar">
@@ -219,19 +143,19 @@ class Home extends React.Component {
                     </div>
                     <div className="toolbarActions">
                         <div>
-                            <ViewModuleIcon onClick={(e) => this.props.updateCurrentViewOption(0, e)}/>
+                            <ViewModuleIcon onClick={(e) => updateCurrentViewOption(0, e)}/>
                         </div>
                         <div>
-                            <ViewListIcon onClick={(e) => this.props.updateCurrentViewOption(1, e)}/>
+                            <ViewListIcon onClick={(e) => updateCurrentViewOption(1, e)}/>
                         </div>
                     </div>
                 </div>
 
                 <div className="homeView">
-                    {this.props.currentViewIndex === -1 ? <div></div> : this.populateHomeView()}
+                    {currentViewIndex === -1 ? <div></div> : this.populateHomeView()}   
                 </div>
 
-                <Backdrop className="backdrop" open={this.props.backdropOpen}>
+                <Backdrop className="backdrop" open={backdropOpen}>
                         <CircularProgress color="inherit" />
                 </Backdrop>
             </div>
@@ -252,10 +176,8 @@ const mapDispatchToProps = (dispatch) => {
         requestAllProjects(currentProjects) {
             console.log(currentProjects);
             if (currentProjects.size === 0) {
-                console.log("inside if");
                 dispatch(actionCreator.getProjects());
             }
-            // currentProjects.length === 0 && dispatch(actionCreator.getProjects());
         },
         updateCurrentViewOption(index) {
             dispatch(actionCreator.updateViewOptions(index));
